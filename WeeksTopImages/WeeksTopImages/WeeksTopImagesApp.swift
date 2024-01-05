7f161e1f152ac4332082adf3e1b13a5efdeac0fb
Shiva Kumar Ponnuraj <shiva.kutt.kumar@gmail.com>
2024-01-05 15:04:10 +0800

    Chore: Add a new Xcode project 	- Added a SwiftUI project 	- Added Unit Testing initial code

---
+++ WeeksTopImages/WeeksTopImages/WeeksTopImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeksTopImagesApp.swift
+//  WeeksTopImages
+//
+//  Created by shiva kumar on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeeksTopImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
