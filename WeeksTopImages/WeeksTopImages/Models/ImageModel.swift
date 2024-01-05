dd3de5734ac91247a4c6562adb8fad41725c4e2f
Shiva Kumar Ponnuraj <shiva.kutt.kumar@gmail.com>
2024-01-05 15:29:40 +0800

    Chore: App code organisation 	- Added folder structure 	- Follows the MVVM architecture structure

---
+++ WeeksTopImages/WeeksTopImages/Models/ImageModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageModel.swift
+//  WeeksTopImages
+//
+//  Created by shiva kumar on 5/1/24.
+//
+
+import Foundation
---
+++ WeeksTopImages/WeeksTopImages/Services/ImageService.swift
@@ -0,0 +1,8 @@
+//
+//  ImageService.swift
+//  WeeksTopImages
+//
+//  Created by shiva kumar on 5/1/24.
+//
+
+import Foundation
