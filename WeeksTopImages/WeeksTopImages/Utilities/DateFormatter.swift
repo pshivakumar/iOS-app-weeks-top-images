0eba67e5f25d115bf54cf3b34082352b3cfb707e
Shiva Kumar Ponnuraj <shiva.kutt.kumar@gmail.com>
2024-01-08 13:14:00 +0800

    Chore: Add DateFormatter Utility - Implemented a localized date formatter with the current locale. - This reusable utility can be used across the app

--- WeeksTopImages/WeeksTopImages/Utilities/DateFormatter.swift
+++ WeeksTopImages/WeeksTopImages/Utilities/DateFormatter.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+extension DateFormatter {
+    /// Creates a localized date formatter with the current locale.
+    ///
+    /// - Returns: A `DateFormatter` instance with the current locale.
+    static func localizedFormatter() -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current
+        return formatter
+    }
+
+    func string(from date: Date, format: String) -> String {
+        self.dateFormat = format
+        return string(from: date)
+    }
+}
